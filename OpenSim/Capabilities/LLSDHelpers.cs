48b9f1e98297d10e1a5f25bcd234d9369b4af92d
EmperorStarfinder <emperor@secondgalaxy.com>
2019-02-28 14:51:54 -0500

    [OpenSim.Capabilities]: Licensing - Security Scan Fix
    - Change the format of the license text to pass the security scans without the scans tossing back issues about the license
    - Organize using references
    
    NOTE: This does not change the text of the license just simply puts it in a format that will pass security scans by Codacy and Coverity without issue.

--- OpenSim/Capabilities/LLSDHelpers.cs
+++ OpenSim/Capabilities/LLSDHelpers.cs
@@ -1,29 +1,31 @@
-/*
- * Copyright (c) Contributors, http://opensimulator.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the OpenSimulator Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <license>
+///     Copyright (c) Contributors, http://opensimulator.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the OpenSimulator Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System;
 using System.Collections;
@@ -36,20 +38,17 @@ namespace OpenSim.Framework.Capabilities
 {
     public class LLSDHelpers
     {
-//        private static readonly log4net.ILog m_log
-//            = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         public static string SerializeLLSDReply(object obj)
         {
             using(StringWriter sw = new StringWriter())
-            using(XmlTextWriter writer = new XmlTextWriter(sw))
+
+            using (XmlTextWriter writer = new XmlTextWriter(sw))
             {
                 writer.Formatting = Formatting.None;
                 writer.WriteStartElement(String.Empty, "llsd", String.Empty);
                 SerializeOSDType(writer, obj);
                 writer.WriteEndElement();
                 writer.Flush();
-            //m_log.DebugFormat("[LLSD Helpers]: Generated serialized LLSD reply {0}", sw.ToString());
 
                 return sw.ToString();
             }
@@ -58,12 +57,12 @@ namespace OpenSim.Framework.Capabilities
         public static string SerializeLLSDReplyNoHeader(object obj)
         {
             using(StringWriter sw = new StringWriter())
-            using(XmlTextWriter writer = new XmlTextWriter(sw))
+
+            using (XmlTextWriter writer = new XmlTextWriter(sw))
             {
                 writer.Formatting = Formatting.None;
                 SerializeOSDType(writer, obj);
                 writer.Flush();
-            //m_log.DebugFormat("[LLSD Helpers]: Generated serialized LLSD reply {0}", sw.ToString());
 
                 return sw.ToString();
             }
@@ -73,6 +72,7 @@ namespace OpenSim.Framework.Capabilities
         {
             Type myType = obj.GetType();
             LLSDType[] llsdattributes = (LLSDType[]) myType.GetCustomAttributes(typeof (LLSDType), false);
+
             if (llsdattributes.Length > 0)
             {
                 switch (llsdattributes[0].ObjectType)
@@ -80,6 +80,7 @@ namespace OpenSim.Framework.Capabilities
                     case "MAP":
                         writer.WriteStartElement(String.Empty, "map", String.Empty);
                         FieldInfo[] fields = myType.GetFields();
+
                         for (int i = 0; i < fields.Length; i++)
                         {
                             if (fields[i] != null && fields[i].GetValue(obj) != null)
@@ -87,6 +88,7 @@ namespace OpenSim.Framework.Capabilities
                                 object fieldValue = fields[i].GetValue(obj);
                                 LLSDType[] fieldAttributes =
                                     (LLSDType[]) fieldValue.GetType().GetCustomAttributes(typeof (LLSDType), false);
+
                                 if (fieldAttributes.Length > 0)
                                 {
                                     writer.WriteStartElement(String.Empty, "key", String.Empty);
@@ -104,8 +106,6 @@ namespace OpenSim.Framework.Capabilities
                                     writer.WriteString(fieldName);
                                     writer.WriteEndElement();
                                     LLSD.LLSDWriteOne(writer, fieldValue);
-                                    // OpenMetaverse.StructuredData.LLSDParser.SerializeXmlElement(
-                                    //    writer, OpenMetaverse.StructuredData.OSD.FromObject(fieldValue));
                                 }
                             }
                             else
@@ -115,19 +115,21 @@ namespace OpenSim.Framework.Capabilities
                                 // if sdague could take a look at this.
                             }
                         }
+
                         writer.WriteEndElement();
                         break;
                     case "ARRAY":
-                        // OSDArray arrayObject = obj as OSDArray;
-                        // ArrayList a = arrayObject.Array;
                         ArrayList a = (ArrayList) obj.GetType().GetField("Array").GetValue(obj);
+
                         if (a != null)
                         {
                             writer.WriteStartElement(String.Empty, "array", String.Empty);
+
                             foreach (object item in a)
                             {
                                 SerializeOSDType(writer, item);
                             }
+
                             writer.WriteEndElement();
                         }
                         break;
@@ -136,8 +138,6 @@ namespace OpenSim.Framework.Capabilities
             else
             {
                 LLSD.LLSDWriteOne(writer, obj);
-                //OpenMetaverse.StructuredData.LLSDParser.SerializeXmlElement(
-                //    writer, OpenMetaverse.StructuredData.OSD.FromObject(obj));
             }
         }
 
@@ -145,30 +145,32 @@ namespace OpenSim.Framework.Capabilities
         {
             Type myType = obj.GetType();
             LLSDType[] llsdattributes = (LLSDType[]) myType.GetCustomAttributes(typeof (LLSDType), false);
+
             if (llsdattributes.Length > 0)
             {
                 switch (llsdattributes[0].ObjectType)
                 {
                     case "MAP":
                         IDictionaryEnumerator enumerator = llsd.GetEnumerator();
+
                         while (enumerator.MoveNext())
                         {
                             string keyName = (string)enumerator.Key;
                             keyName = keyName.Replace("-","_");
                             FieldInfo field = myType.GetField(keyName);
+
                             if (field != null)
                             {
-                                // if (enumerator.Value is OpenMetaverse.StructuredData.OSDMap)
                                 if (enumerator.Value is Hashtable)
                                 {
                                     object fieldValue = field.GetValue(obj);
                                     DeserializeOSDMap((Hashtable) enumerator.Value, fieldValue);
-                                    //  DeserializeOSDMap((OpenMetaverse.StructuredData.OSDMap) enumerator.Value, fieldValue);
                                 }
                                 else if (enumerator.Value is ArrayList)
                                 {
                                     object fieldValue = field.GetValue(obj);
                                     fieldValue.GetType().GetField("Array").SetValue(fieldValue, enumerator.Value);
+                                    
                                     //TODO
                                     // the LLSD map/array types in the array need to be deserialized
                                     // but first we need to know the right class to deserialize them into.
@@ -198,6 +200,7 @@ namespace OpenSim.Framework.Capabilities
                         break;
                 }
             }
+
             return obj;
         }
     }
--- OpenSim/Capabilities/LLSDStreamHandler.cs
+++ OpenSim/Capabilities/LLSDStreamHandler.cs
@@ -1,29 +1,31 @@
-/*
- * Copyright (c) Contributors, http://opensimulator.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the OpenSimulator Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <license>
+///     Copyright (c) Contributors, http://opensimulator.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the OpenSimulator Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System.Collections;
 using System.IO;
@@ -33,8 +35,7 @@ using OpenSim.Framework.Servers.HttpServer;
 
 namespace OpenSim.Framework.Capabilities
 {
-    public class LLSDStreamhandler<TRequest, TResponse> : BaseStreamHandler
-        where TRequest : new()
+    public class LLSDStreamhandler<TRequest, TResponse> : BaseStreamHandler where TRequest : new()
     {
         private LLSDMethod<TRequest, TResponse> m_method;
 
@@ -48,21 +49,14 @@ namespace OpenSim.Framework.Capabilities
             m_method = method;
         }
 
-        protected override byte[] ProcessRequest(string path, Stream request,
-                                      IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        protected override byte[] ProcessRequest(string path, Stream request, IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
         {
-            //Encoding encoding = Util.UTF8;
-            //StreamReader streamReader = new StreamReader(request, false);
-
-            //string requestBody = streamReader.ReadToEnd();
-            //streamReader.Close();
-
-            // OpenMetaverse.StructuredData.OSDMap hash = (OpenMetaverse.StructuredData.OSDMap)
-            //    OpenMetaverse.StructuredData.LLSDParser.DeserializeXml(new XmlTextReader(request));
-
             Hashtable hash = (Hashtable) LLSD.LLSDDeserialize(request);
-            if(hash == null)
+
+            if (hash == null)
+            {
                 return new byte[0];
+            }
 
             TRequest llsdRequest = new TRequest();
             LLSDHelpers.DeserializeOSDMap(hash, llsdRequest);
